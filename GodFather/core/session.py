6eb7678dd57f43659aff21d6099bb5d9f6b42fd3
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-07-30 09:00:21 -0700

    Add files via upload

---
+++ GodFather/core/session.py
@@ -0,0 +1,42 @@
+import sys
+
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+from telethon.sessions import StringSession
+
+from ..Config import Config
+from .client import godfatherClient
+
+__version__ = "1.10.6"
+
+loop = None
+
+if Config.godfather_STRING:
+    session = StringSession(str(Config.godfather_STRING))
+else:
+    session = "godfatherUserBot"
+
+try:
+    godfather = godfatherClient(
+        session=session,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except Exception as e:
+    print(f"godfather_STRING - {e}")
+    sys.exit()
+
+godfather.tgbot = tgbot = godfatherClient(
+    session="godfatherTgbot",
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    loop=loop,
+    app_version=__version__,
+    connection=ConnectionTcpAbridged,
+    auto_reconnect=True,
+    connection_retries=None,
+).start(bot_token=Config.BOT_TOKEN)
